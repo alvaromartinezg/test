ccbaaec113d0f720f03a1b51d869e4d6c9cdb240
alvaro.martinez <alvandremg@gmail.com>
2025-10-20 09:37:30 -0500

    Delete js/config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-// Renombra este archivo a config.js y ajusta los endpoints.
-// Si usas Git, añade 'js/config.js' al .gitignore.
-
-window.ENDPOINTS = {
-  PROCESS_URL: "https://kmz-processor-411960240345.us-central1.run.app/process",
-  CONVERT_URL: "https://kmz2cad-411960240345.us-central1.run.app/process"
-};
